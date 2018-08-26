2aeb1867dd349c0fc48843b44a3d4470a5f1a059
kekonen <daniil.naumetc@gmail.com>
2018-08-26 13:08:19 +0200

    auth cool

--- src/main.rs
+++ src/main.rs
@@ -18,22 +18,46 @@ struct LoginFormInput<'ab> {
 
 struct UserAdmin(String);
 
+// fn in_array(arr: &Vec<String>, val: &String) -> bool{
+// 	let found = arr.iter().enumerate().find(|&user| user.1.to_string() == *val);
+//     match found {
+//         // Match a single value
+//         Some(x) => true,
+//         _ => false,
+//     }
+// }
+
 impl<'a, 'r> FromRequest<'a, 'r> for UserAdmin {
     type Error = ();
 
     fn from_request(request: &'a Request<'r>) -> request::Outcome<UserAdmin, ()> {
+		let mut admins = vec!["andrey".to_string(), "michael".to_string(), "kekkonen".to_string()];
+
+    	let found_admin = request.cookies()
+            .get_private("user_id")
+            .and_then(|cookie| cookie.value().parse().ok())
+            .map(|id: String| admins.iter().find(|&user| user.to_string() == id));
+            // .map(|id: String| in_array(&admins, &id));
+            // .map(|id: String| admins.iter().enumerate().find(|&user| user.1.to_string() == id));
+            
+            // .or_forward(())
         // let keys: Vec<_> = request.headers().get("x-api-key").collect();
         // if keys.len() != 1 {
         //     return Outcome::Failure((Status::BadRequest, ()));
         // } 
 
-
-        // let key = keys[0];
-        // if !is_valid(keys[0]) {
+        match found_admin {
+	        // Match a single value
+	        Some(user) => {
+	        	match user {
+	        		Some(admin) => Outcome::Success(UserAdmin(admin.to_string())),
+	        		_ => Outcome::Forward(()),
+	        	} 
+	        },
+	        _ => Outcome::Failure((Status::BadRequest, ())),
+	    }
         //     return Outcome::Forward(());
-        // }
-
-        return Outcome::Success(UserAdmin("AdminKey".to_string()));
+        // return Outcome::Success(UserAdmin("AdminKey".to_string()));
     }
 }
 
@@ -48,11 +72,11 @@ fn check(mut cookies: Cookies) -> Option<String> {
         .map(|cookie| format!("User ID: {}", cookie.value()))
 }
 
-#[get("/cookies")]
-fn get_cookies(mut cookies: Cookies) -> String {
-    cookies.add_private(Cookie::new("user_id", "kekkonen"));
-    format!("Created a cookie for you!")
-}
+// #[get("/cookies")]
+// fn get_cookies(mut cookies: Cookies) -> String {
+//     cookies.add_private(Cookie::new("user_id", "kekkonen"));
+//     format!("Created a cookie for you!")
+// }
 
 #[get("/logout")]
 fn logout(mut cookies: Cookies) -> Flash<Redirect> {
@@ -72,10 +96,12 @@ fn login() -> io::Result<NamedFile> {
 }
 
 #[post("/login", data = "<login_input>")]
-fn login_submit<'r>(login_input: Form<'r, LoginFormInput<'r>>) -> io::Result<NamedFile> {
+fn login_submit<'r>(login_input: Form<'r, LoginFormInput<'r>>, mut cookies: Cookies) -> String {
     println!("Hello, {}, with password {}!", login_input.get().login, login_input.get().password);
-    NamedFile::open("client/pages/login.html")
-    // Return session
+    // check if user is alright
+    cookies.add_private(Cookie::new("user_id", login_input.get().login.to_string()));
+    format!("Created a cookie for you!")
+    // NamedFile::open("client/pages/login.html")
 }
 
 #[get("/src/<file..>")]
@@ -90,5 +116,5 @@ fn secret_info(admin: UserAdmin) -> String {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, files, login, login_submit, secret_info, check, logout, get_cookies]).launch();
+    rocket::ignite().mount("/", routes![index, files, login, login_submit, secret_info, check, logout]).launch();
 }
